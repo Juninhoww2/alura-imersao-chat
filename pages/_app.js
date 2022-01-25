f28edb85a7c9fecc549f085174d36a80b975c100
juninhoww2 <alqadianokira@gmail.com>
2022-01-25 09:54:51 -0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,38 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      list-style: none;
+    }
+    body {
+      font-family: 'Open Sans', sans-serif;
+    }
+    /* App fit Height */ 
+    html, body, #__next {
+      min-height: 100vh;
+      display: flex;
+      flex: 1;
+    }
+    #__next {
+      flex: 1;
+    }
+    #__next > * {
+      flex: 1;
+    }
+    /* ./App fit Height */ 
+  `}</style>
+  );
 }
 
-export default function App({ Component, pageProps }) {
+export default function CustomApp({ Component, pageProps }) {
+  console.log('Roda em todas as páginas!');
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import appConfig from '../config.json'
+import React from 'react'
+import { useRouter } from 'next/router'
 
-function GlobalStyle() {
-  return (
-    <style global jsx>
-      {`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */           
-      `}
-    </style>
-  )
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +32,12 @@ function Titulo(props) {
 // }
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  //  const username = 'peas';
+  const [username, setUsername] = React.useState('omariosouto')
+  const roteamento = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +64,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguem submeteu o form')
+              roteamento.push('/chat')
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,6 +80,13 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value)
+                const valor = event.target.value;
+
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
